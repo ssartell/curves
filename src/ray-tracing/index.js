bdd76eaa85012df553aa992e3650c033b91da196
ssartell <ssartell@rightpoint.com>
2018-03-05 10:25:30 -0600

    update raytracer

--- src/ray-tracing/index.js
+++ src/ray-tracing/index.js
@@ -12,6 +12,12 @@ var random = xy => Math.random() * 10;
 var min = R.reduce(R.min, Infinity);
 var max = R.reduce(R.max, -Infinity);
 
+var surfaces = {
+    slightlyRough: (normal, point) => vec.normalize(R.map(x => x + Math.random() / 20, normal)),
+    veryRough: (normal, point) => vec.normalize(R.map(x => x + Math.random() / 5, normal)),
+    smooth: (normal, point) => normal
+};
+
 var scene = {
     camera: {
         position: [0, 0, 0],
@@ -19,6 +25,11 @@ var scene = {
         fov: 90
     },
     shapes: [
+        {
+            type: 'plane',
+            point: [0, -5, 0],
+            normal: [0, 1, 0]
+        },
         {
             type: 'sphere',
             center: [-1, 0, 4],
@@ -33,30 +44,30 @@ var scene = {
             ambient: [.25, .20, .07],
             diffuse: [.75, .61, .23],
             specular: [.62, .55, .63],
-            exponent: 27.8
+            exponent: 27.8,
+            surface: surfaces.smooth
         },
         {
             type: 'sphere',
             center: [1, .5, 3],
             radius: .5,
-            // silver
-            // ambient: [.19, .19, .19],
-            // diffuse: [.51, .51, .51],
-            // specular: [.51, .51, .51],
             // exponent: 51.2
             ambient: [.19, .07, .02],
             diffuse: [.7, .27, .08],
             specular: [.25, .13, .08],
-            exponent: 12.8
+            exponent: 12.8,
+            surface: surfaces.veryRough
         },
         {
             type: 'sphere',
             center: [.35, -.35, 1.5],
             radius: .5,
+            // silver
             ambient: [.19, .19, .19],
             diffuse: [.51, .51, .51],
             specular: [.51, .51, .51],
-            exponent: 51.2
+            exponent: 51.2,
+            surface: surfaces.slightlyRough
         }
     ],
     lights: [
@@ -73,7 +84,7 @@ var scene = {
             intensity: [.25, 0, 0]
         }
     ],
-    ambient: [.1, .1, .1]
+    ambient: [.1, .1, .5]
 };
 
 var raytrace = (function () {
@@ -106,40 +117,58 @@ var raytrace = (function () {
 
     function traceRay(scene, ray, depth) {
         depth = depth || 0;
-        if (depth > 5) return [0, 0, 0];
+        if (depth > 3) return scene.ambient;
 
-        var intersections = scene.shapes
-            .map(shape => intersectShape(ray, shape))
-            .filter(x => Number.isFinite(x.t))
-            .sort((a, b) => a.t - b.t);
-
-        if (intersections.length === 0) return [0, 0, 0];
-        var intersection = intersections[0];
+        var intersection = intersectShapes(scene, ray);
+        if (!intersection) return scene.ambient;
 
         var lighting = colorAtIntersection(scene, intersection);
 
-        var reflectedRay = vec.reflect(ray.vector, intersection.normal);
+        var reflectedRay = {
+            point: intersection.pointAtTime,
+            vector: vec.reflect(ray.vector, intersection.normal)
+        };
 
-        var reflection = traceRay(scene, { point: intersection.pointAtTime, vector: reflectedRay }, ++depth);
+        var reflection = traceRay(scene, reflectedRay, ++depth);
 
         return vec.add(lighting, vec.multiply(intersection.shape.specular, reflection));
     }
 
-    function intersectShape(ray, shape) {
+    function intersectShapes(scene, ray) {
+        var intersections = scene.shapes
+            .map(shape => {
+                switch (shape.type) {
+                    case 'sphere':
+                        return intersectSphere(ray, shape);
+                    case 'plane':
+                        return intersectPlane(ray, shape);
+                }
+            })
+            .filter(x => x !== null && Number.isFinite(x.t))
+            .sort((a, b) => a.t - b.t);
+
+        if (intersections.length === 0) return null;
+        return intersections[0];
+    }
+
+    function intersectPlane(ray, plane) {
+        return null;
+    }
+
+    function intersectSphere(ray, shape) {
         var L = vec.subtract(ray.point, shape.center);
         var a = vec.dotProduct(ray.vector, ray.vector);
         var b = 2 * vec.dotProduct(ray.vector, L);
         var c = vec.dotProduct(L, L) - shape.radius * shape.radius;
 
         var t = solveQuadratic(a, b, c);
-        if (!Number.isFinite(t) || t < .001) 
-            return { t: Infinity };
+        if (!Number.isFinite(t) || t < .001)
+            return null;
 
         var pointAtTime = vec.add(ray.point, vec.scale(ray.vector, t));
-        var normal = vec.normalize(vec.subtract(pointAtTime, shape.center));
 
-        // perturb the normal a bit
-        normal = vec.normalize(R.map(x => x + Math.random() / 20, normal));
+        var normal = vec.normalize(vec.subtract(pointAtTime, shape.center));
+        normal = shape.surface(normal, pointAtTime);
 
         return { shape, t, pointAtTime, normal };
     }
@@ -150,7 +179,7 @@ var raytrace = (function () {
         var pointToCamera = vec.normalize(vec.subtract(scene.camera.position, intersection.pointAtTime));
         var shape = intersection.shape;
         var normal = intersection.normal;
-        var upCos = vec.dotProduct(normal, [0,1,0]);
+        var upCos = vec.dotProduct(normal, [0, 1, 0]);
 
         for (var light of scene.lights) {
             var pointToLight = vec.normalize(vec.subtract(light.position, intersection.pointAtTime));
@@ -204,16 +233,19 @@ function renderCanvas(width, height, f) {
     c.height = height;
     //c.style.cssText = `width: ${width * 2}px; height: ${height * 2}px`;
 
-    var ctx = c.getContext('2d'),
-        data = ctx.getImageData(0, 0, width, height);
+    var ctx = c.getContext('2d');
+    var data = ctx.getImageData(0, 0, width, height);
+    ctx.webkitImageSmoothingEnabled = false;
+    ctx.mozImageSmoothingEnabled = false;
+    ctx.imageSmoothingEnabled = false;
 
     var i = 0;
 
     for (var y = 0; y < height; y++) {
         for (var x = 0; x < width; x++) {
             var color = f([x, y]);
-            i = (x * 4) + (y * width * 4),
-                data.data[i + 0] = color[0];
+            i = (x * 4) + (y * width * 4);
+            data.data[i + 0] = color[0];
             data.data[i + 1] = color[1];
             data.data[i + 2] = color[2];
             data.data[i + 3] = 255;
@@ -221,6 +253,11 @@ function renderCanvas(width, height, f) {
     }
 
     ctx.putImageData(data, 0, 0);
+}
+
+function createGrid(n, m, f) {
+    var arr = R.range(0, n).map(x => R.range(0, m));
+    return arr.map((_, y) => arr.map((_, x) => f([x, y])));
 }
 
 function renderGrid(grid) {
@@ -243,11 +280,6 @@ function renderGrid(grid) {
         .attr('x', (d, i) => `${i / width * 100}%`)
         .attr('width', d => `${1 / width * 100}%`)
         .attr('height', d => `${1 / height * 100}%`);
-}
-
-function createGrid(n, m, f) {
-    var arr = R.range(0, n).map(x => R.range(0, m));
-    return arr.map((_, y) => arr.map((_, x) => f([x, y])));
 }
 
 function renderContour(grid, thresholds) {
